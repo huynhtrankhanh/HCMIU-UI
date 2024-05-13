a3c5eeb798e9f9cb29f5b5983a02963571dbd0ba
Huỳnh Trần Khanh <qcdz9r6wpcbh59+github@gmail.com>
2024-05-13 17:50:05 +0700

    Use ENA pin

--- HCMIU-UI.ino
+++ HCMIU-UI.ino
@@ -1,53 +1,56 @@
 // Define L298N class
 class L298N {
   private:
-    int _pin1;
-    int _pin2;
-    int _intensity;
+    int _ENA;
+    int _IN1;
+    int _IN2;
     
   public:
     // Constructor
-    L298N(int pin1, int pin2) {
-      _pin1 = pin1;
-      _pin2 = pin2;
-      pinMode(_pin1, OUTPUT);
-      pinMode(_pin2, OUTPUT);
+    L298N(int ENA, int IN1, int IN2) {
+      _ENA = ENA;
+      _IN1 = IN1;
+      _IN2 = IN2;
+      pinMode(_ENA, OUTPUT);
+      pinMode(_IN1, OUTPUT);
+      pinMode(_IN2, OUTPUT);
     }
     
     // Method to set intensity (speed)
     void setIntensity(int intensity) {
-      _intensity = intensity;
+      analogWrite(_ENA, intensity);
     }
     
     // Method to set direction
     void setDirection(bool forward) {
       if(forward) {
-        digitalWrite(_pin1, HIGH);
-        analogWrite(_pin2, _intensity);
+        digitalWrite(_IN1, HIGH);
+        digitalWrite(_IN2, LOW);
       } else {
-        digitalWrite(_pin1, LOW);
-        analogWrite(_pin2, _intensity);
+        digitalWrite(_IN1, LOW);
+        digitalWrite(_IN2, HIGH);
       }
     }
 };
 
-// Instantiate two L298N objects for pumps
-L298N pump1(3, 5); // Assuming pins 3 and 5 are used for pump1
-L298N pump2(6, 9); // Assuming pins 6 and 9 are used for pump2
+// Instantiate two L298N objects for motors
+L298N motor1(10, 3, 5); // Assuming ENA is pin 10, IN1 is pin 3, and IN2 is pin 5 for motor1
+L298N motor2(11, 6, 9); // Assuming ENA is pin 11, IN1 is pin 6, and IN2 is pin 9 for motor2
 
 void setup() {
   // Set max intensity
-  pump1.setIntensity(255);
-  pump2.setIntensity(255);
+  motor1.setIntensity(255);
+  motor2.setIntensity(255);
   
-  // Set direction to forward for both pumps
-  pump1.setDirection(true);
-  pump2.setDirection(true);
+  // Set direction to forward for both motors
+  motor1.setDirection(true);
+  motor2.setDirection(true);
 }
 
 void loop() {
-  // Control pumps using methods from L298N class
+  // Control motors using methods from L298N class
   // Example: 
-  // pump1.setIntensity(128);
-  // pump2.setDirection(false);
+  // motor1.setIntensity(128);
+  // motor2.setDirection(false);
+  // ...
 }
